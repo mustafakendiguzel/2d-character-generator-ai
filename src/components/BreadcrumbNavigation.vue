a260a12292febe4db07d121e98ad8ccba6b812b9
mustafakendiguzel <mustafakendiguzel@gmail.com>
2024-10-01 19:38:54 +0300

    feat: change pages

--- src/components/BreadcrumbNavigation.vue
+++ src/components/BreadcrumbNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex mb-4 ml-8" aria-label="Breadcrumb">
+  <nav class="flex mb-4 ml-18" aria-label="Breadcrumb">
     <ol class="inline-flex items-center space-x-1 md:space-x-3">
       <li v-for="(item, index) in breadcrumbs" :key="index" class="inline-flex items-center">
         <router-link
